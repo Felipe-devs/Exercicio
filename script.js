313998242dc1efe42706f37eec15c7140139ddaf
Felipe <felipe_fifa@hotmail.com>
2021-04-01 21:54:55 -0300

    Criado Calculadora

---
+++ script.js
@@ -0,0 +1,15 @@
+function call() {
+    var inputs = document.getElementsByTagName('input')
+    var input2 = inputs[0];
+    var input1 = inputs[0]
+    var soma = parsefloat.input.value();
+
+    if(soma === '') {
+        alert( 'Insira o números');
+return;
+    }
+
+    console.log('Nome: ', nome);
+    window.alert('Hello World, ' + nome + '!');
+    input.value= '';
+}
